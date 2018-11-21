73f4fd513d64d30977ba51ea20381b068a95ef52
Adam <adam@iii.com>
2018-11-22 00:01:11 +0800

    jsf navigation with choices

--- hello-java-web/src/main/java/jsf/ui/NavigationBean.java
+++ hello-java-web/src/main/java/jsf/ui/NavigationBean.java
@@ -1,6 +1,7 @@
 package jsf.ui;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
 
 @ManagedBean
@@ -9,6 +10,11 @@ public class NavigationBean {
 	
 	private String name;
 	private String action;
+	
+	@ManagedProperty(value="#{param.param}")
+	private String param;
+	@ManagedProperty(value="#{param.param2}")
+	private String param2;
 	
 	public String getName() {
 		return name;
@@ -22,13 +28,34 @@ public class NavigationBean {
 	public void setAction(String action) {
 		this.action = action;
 	}
-	
+	public String getParam() {
+		return param;
+	}
+	public void setParam(String param) {
+		this.param = param;
+	}
+	public String getParam2() {
+		return param2;
+	}
+	public void setParam2(String param2) {
+		this.param2 = param2;
+	}
 	public String view() {
 		return "form";
 	}
 	
 	public String redir() {
 		return "red";
+	}
+	
+	public String choice() {
+		System.out.println(param);
+		System.out.println(param2);
+		if(param2==null) {
+			return "/jsf/2/nav?faces-redirect=true";
+		}else {
+			return "nav";
+		}
 	}
 
 }
