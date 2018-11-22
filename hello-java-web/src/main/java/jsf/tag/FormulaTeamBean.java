cfb94f67c2e19d076b2f4dd7599d71a1d89db185
Adam <adam@iii.com>
2018-11-23 01:08:29 +0800

    jsf datatable

--- hello-java-web/src/main/java/jsf/tag/FormulaTeamBean.java
+++ hello-java-web/src/main/java/jsf/tag/FormulaTeamBean.java
@@ -1,7 +1,13 @@
 package jsf.tag;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
 public class FormulaTeamBean {
 	
 	private String name;
@@ -9,6 +15,30 @@ public class FormulaTeamBean {
 	private int year;
 	private Date date;
 	private long rpm;
+	
+	private static final List<FormulaTeamBean> list=new ArrayList<>(
+			Arrays.asList(
+					new FormulaTeamBean("kimi","Woking",2002,new Date(),18200),
+					new FormulaTeamBean("ralf","Wantage",2003,new Date(),19000),
+					new FormulaTeamBean("mika","Silverstone",1999,new Date(),17400),
+					new FormulaTeamBean("pastor","Troll",2012,new Date(),18000),
+					new FormulaTeamBean("alonso","Enstone",2005,new Date(),18800)
+			));
+	
+	public List<FormulaTeamBean> getList() {
+		return list;
+	}
+	
+	public FormulaTeamBean(String name,String location,int year,Date date,long rpm) {
+		this.name=name;
+		this.location=location;
+		this.year=year;
+		this.date=date;
+		this.rpm=rpm;
+	}
+	public FormulaTeamBean() {
+		
+	}
 	
 	public String getName() {
 		return name;
@@ -39,6 +69,16 @@ public class FormulaTeamBean {
 	}
 	public void setRpm(long rpm) {
 		this.rpm = rpm;
+	}
+	
+	public List<FormulaTeamBean> getTableBean(){
+		return Arrays.asList(
+				new FormulaTeamBean("kimi","Woking",2002,new Date(),18200),
+				new FormulaTeamBean("ralf","Wantage",2003,new Date(),19000),
+				new FormulaTeamBean("mika","Silverstone",1999,new Date(),17400),
+				new FormulaTeamBean("pastor","Troll",2012,new Date(),18000),
+				new FormulaTeamBean("alonso","Enstone",2005,new Date(),18800)
+		);
 	}
 
 }
